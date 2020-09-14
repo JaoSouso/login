be39f469c5861223f53e6e270327b637756e5f8f
JaoSouso <70705178+JaoSouso@users.noreply.github.com>
2020-09-14 12:13:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<script data-ad-client="ca-pub-9619616969215009" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - SecFlix</title>
+    <link rel="stylesheet" href="css/indexstyle.css">
+    <link rel="shortcut icon" href="images/images_1.jpg">
+</head>
+<body>
+    <h1 class="titulo">Veja filmes de graça</h1>
+</body>
+</html>
